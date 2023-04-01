a2f981d5dc08960bbe13982a934815cee6167886
harold mei <haroldmei.cn@gmail.com>
2023-04-01 19:49:43 +1100

    how about now

--- src/forms/mod1.py
+++ src/forms/mod1.py
@@ -100,7 +100,7 @@ class mod1(form_base):
             print('\n\n ================ personal info ================= \n', students[-1][3])
             print('\n\n ================= edu info: ================= \n', students[-1][1])
             print('\n\n ================= application info: ================= \n', students[-1][2])
-            
+
         return self.main_application_handle
 
     def fill_personal_info(self):
@@ -159,7 +159,7 @@ class mod1(form_base):
         self.set_value(parent_address1, personal_info['line1'])
         self.set_value(parent_address2, personal_info['line2'])
         self.set_value(parent_address3, personal_info['line3'])
-        self.set_value(parent_town, personal_info['city'])//*[@id="form_email_applicant"]
+        self.set_value(parent_town, personal_info['city'])
         self.set_value(parent_state, personal_info['province'])
         self.set_value(parent_postcode, post)
 
@@ -174,7 +174,7 @@ class mod1(form_base):
         nationality = '/html/body/div[1]/form/div[10]/div[2]/div/div/div[4]/div/div/div/div/input'
         is_aboriginal = '/html/body/div[1]/form/div[10]/div[2]/div/div/div[7]/div/div/div/div/input'
         hometongue = '/html/body/div[1]/form/div[10]/div[2]/div/div/div[8]/div/div/div/div/input'
-        //*[@id="form_email_applicant"]
+        
         self.set_value_list(born_country, 'China (Excludes SARS and Taiwan)')
         self.set_value_list(au_citizenship, 'Other (non resident)')
         self.set_value_list(nationality, 'China (Excludes SARS and Taiwan)')
